c16bc5badb4d4f4189ec239264bcd21e789f4613
Philip Snyder <psnyder385@gmail.com>
2025-03-24 21:13:54 -0400

    test

--- index.ts
+++ index.ts
@@ -4,7 +4,7 @@ const app = express();
 const port = 3000;
 
 app.get("/", (_req, res) => {
-  res.send("Hello from Bun + Express!");
+  res.send("Hello from Bun + Express2!");
 });
 
 app.listen(port, () => {
